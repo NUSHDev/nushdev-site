c325c1f36d94604251567006ee1cc432a5fb3cf5
skibidi sigma developer <68497851+nushhw@users.noreply.github.com>
2025-06-20 18:09:41 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+document.getElementById("body").onscroll = function myFunction() {  
+    var scrolltotop = document.scrollingElement.scrollTop;
+    var target = document.getElementById("main1");
+    var xvalue = "center";
+    var factor = 0.5;
+    var yvalue = scrolltotop * factor;
+    target.style.backgroundPosition = xvalue + " " + yvalue + "px";
+} //new changes lol
 document.addEventListener('DOMContentLoaded', () => {
   const faders = document.querySelectorAll('.fade-in');
 
